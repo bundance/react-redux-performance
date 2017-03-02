a1b0362415b3ee2ed8a1269b375daa7e9f82f594
Mike Evans <michael.evans@bgch.co.uk>
2017-03-02 18:27:03 +0000

    add shouldComponentUpdate to todo.component and stop all wasted re-renderings

--- src/components/todo/todo.component.js
+++ src/components/todo/todo.component.js
@@ -1,16 +1,26 @@
 import React, { PropTypes } from 'react';
 import './todo.css' 
 
-Todo.proptypes = {
-    todo: PropTypes.object.isRequired
-};
+export default class Todo extends React.Component {
+    static propTypes = {
+        todo: PropTypes.object
+    };
 
-export default function Todo({
-    todo
-})  {
-    return (
-        <span className={todo.completed ? 'completed' : 'uncompleted'}>
-            ToDo: {todo.text}
-        </span>
-    )
+    // This stop unnecessary todo renders
+    shouldComponentUpdate(nextProps) {
+        return !(nextProps.todo.completed === this.props.todo.completed
+            && nextProps.todo.id === this.props.todo.id
+            && nextProps.todo.text === this.props.todo.text)
+    }
+
+    render() {
+
+        const { todo } = this.props;
+
+        return (
+            <span className={todo.completed ? 'completed' : 'uncompleted'}>
+                ToDo: {todo.text}
+            </span>
+        )
+    }
 }
