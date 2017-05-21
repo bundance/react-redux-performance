7fd60bc008d2dfc6ac71e25305152f51d0530a73
Mike Evans <bundance@users.noreply.github.com>
2017-05-21 13:45:50 +0100

    remove memoized todo-list selectors

--- src/components/todo/todo.component.js
+++ src/components/todo/todo.component.js
@@ -1,4 +1,5 @@
 import React, { PropTypes } from 'react';
+import shallowCompare from 'react-addons-shallow-compare';
 import './todo.css'
 
 export default class Todo extends React.Component {
@@ -12,60 +13,13 @@ export default class Todo extends React.Component {
         this.state = {};
     }
     
-    shouldComponentUpdate(nextProps, nextState, nextContext) {
-        const propsNotEqual = !this.shallowEqual(nextProps, this.props);
-        const stateNotEqual = !this.shallowEqual(nextState, this.state);
-        const contextNotEqual = !this.shallowEqual(nextContext, this.context);
+    shouldComponentUpdate(nextProps, nextState) {
+        // shouldUpdate is true if the component should update (i.e. nextProps or nextState is different from
+        // this.props or this.state)
+        const shouldUpdate = shallowCompare(this, nextProps, nextState);
+        console.log(`Todo ${this.props.todo.id} shouldUpdate is ${ shouldUpdate }`);
 
-        const retVal = propsNotEqual || stateNotEqual || contextNotEqual;
-
-        console.log(`Todo ${this.props.todo.id} is ${ retVal }`);
-
-        return retVal;
-    }
-
-    shallowEqual(objA, objB, log=false) {
-        if (Object.is(objA, objB)) {
-            return true;
-        }
-
-        if (typeof objA !== 'object' || objA === null || typeof objB !== 'object' || objB === null) {
-            if(log){
-                console.log('**** typeof obj returns FALSE');
-            }
-            return false;
-        }
-
-        var keysA = Object.keys(objA);
-        var keysB = Object.keys(objB);
-
-        if (keysA.length !== keysB.length) {
-            if(log) {
-                console.log('**** keylength returns FALSE');
-            }
-            return false;
-        }
-
-        // Test for A's keys different from B.
-        for (var i = 0; i < keysA.length; i++) {
-            if(log) {
-                console.log('::::::::: id: ', this.props.todo.id);
-                // console.log('objB: ', objB, 'keysA[i]:', keysA[i]);
-                // console.log('Object.hasOwnProperty.call(objB, keysA[i]):', Object.hasOwnProperty.call(objB, keysA[i]));
-                console.log('objA[keysA[i]]:', objA[keysA[i]], ', objB[keysA[i]]: ', objB[keysA[i]]);
-                console.log('!is(objA[keysA[i]], objB[keysA[i]]): ', !Object.is(objA[keysA[i]], objB[keysA[i]]));
-            }
-            if (!Object.hasOwnProperty.call(objB, keysA[i]) || !Object.is(objA[keysA[i]], objB[keysA[i]])) {
-                if(log) {
-                    console.log('**** hasOwnProps returns FALSE');
-                }
-                return false;
-            }
-        }
-        if(log) {
-            console.log('**** typeof obj returns TRUE');
-        }
-        return true;
+        return shouldUpdate;
     }
 
     render() {
--- src/state/todo-list/todo-list.selectors.js
+++ src/state/todo-list/todo-list.selectors.js
@@ -1,52 +1,21 @@
-import { compose, memoize, values } from 'ramda';
+import { compose, values } from 'ramda';
 import { TODO_LIST, TODOS } from '../../constants/todo-list/todo-list.constants';
-import { createSelector } from 'reselect';
 
 /******************************************* SELECTORS *******************************************/
 
 /**
- * Memoizing the getAllTodos() selector will ensure that when mapStateToProps calls it in the ToDoList connected
- * component (defined in todo-list.container), it will not trigger a wasted render if the state.todoList.todos
- * object has not changed.
+ * selectToDos returns a hash of todo objects, keyed by id.
+ * compose(values, selectToDos) returns an array of the same todo objects
  *
- * Conversely, when using the Unmemoized selectors in mapStateToProps, mapStateToProps WILL trigger a re-render every
- * time it's called, even if the values of the todos themselves have not changed.
-
- * Note that if you have a working shouldComponentUpdate() method in your component classes, the re-render will be
- * prevented from occurring, and so you won't see any wasteful re-rendering reported by React-perf.
- *
- * However, you *will* see the Why-Did-You-Update HoC report that a wasteful render was triggered, which tells you
- * that mapStateToProps is creating new objects each time.
- *
- * This is worth repeating: shouldComponentUpdate() will stop a wasteful re-render from mapStateToProps, and so stop
- * react-perf from reporting an unnecessary *data generation*
- *
- * In contrast, WhyDidYouUpdate *will* report any wasteful data regenerations in mapStateToProps, but this will only
- * lead to wasteful re-rendering if shouldComponentUpdate() isn't doing its job.
+ * Each time getAllTodos is called by todo-list.container's mapStateToProps, a new array is returned by ramda's
+ * value() function. However, that array always comprises the same todo objects (so long as they haven't been mutated),
+ * and so Todo.component will not re-render, as its todo object passed into it has not changed.
  *
  */
 
 const selectToDos = state => state[TODO_LIST][TODOS];
 
-
-// Memoized selector
-// Converts an object of todos keyed by id into an array of objects with the
-// properties { id, text, completed }. The todos object returned from selectToDos() is memoized, and so if the same
-// object is returned by selectTodos() on a subsequent call, the same, previously memoized, array is returned.
-export const getAllTodosMemoized = createSelector(
-    [compose(values, selectToDos)],
-    todos => todos
-);
-
-
-// Unmemoized selector
-// Converts an object of todos keyed by id into an array of objects with the
-// properties { id, text, completed } A new array of the same objects is returned each time this selector is called.
-export const getAllTodosUnmemoized = compose(values, selectToDos);
-
 export default {
-    // change this to getAllTodosUnmemoized or getAllTodosMemoized, depending on whether you want to use an 
-    // unmemoized or memoized selector
-    getAllTodos: getAllTodosMemoized
+    getAllTodos: compose(values, selectToDos)
 };
 
